f993ca2b1955042ba6f1821f8d9e62bca4a606cf
Gabriel Siqueira <125387064+ToastedGMS@users.noreply.github.com>
2023-09-09 22:34:20 -0300

    added function for displaying read status and improved comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Initialize the library with some initial books (dummy data for testing).
 const myLibrary = [
     {
         "title": "Bible",
-        "author": "God ",
+        "author": "God",
         "year": "2002"
     },
     {
@@ -9,44 +10,50 @@ const myLibrary = [
         "author": "The Rock",
         "year": "2022"
     }
-    //contains dummy objects for test purposes that SHOULD be deleted later
+    //SHOULD be deleted later
 ];
 
+// Book constructor function to create book objects.
 function Book(title, author, year) {
     this.title = title;
     this.author = author;
     this.year = year;
-  // the constructor...
+    this.read = false; 
 }
+
 const subBtn = document.querySelector('.mainBtn');
+
+// Function to add a book to the library.
 function addBookToLibrary() {
     const bookInfo = document.querySelector('#bookInfo');
-    // creates the button that will access the user's input
+    event.preventDefault();
 
-        event.preventDefault();
-        const title = document.querySelector('#Title');
-        const author = document.querySelector('#Author');
-        const year = document.querySelector('#Year');
+    const title = document.querySelector('#Title');
+    const author = document.querySelector('#Author');
+    const year = document.querySelector('#Year');
 
-        if (title.value === '' || author.value === '' || year.value === ''){
-            alert('Please fill all fields');
-        }else {
-        const newBook = new Book(title.value , author.value , year.value);
+    // Check if any required field is empty and show an alert if so.
+    if (title.value === '' || author.value === '' || year.value === '') {
+        alert('Please fill all fields');
+    } else {
+        // Create a new Book object and add it to the library.
+        const newBook = new Book(title.value, author.value, year.value);
         myLibrary.push(newBook);
         bookInfo.reset();
     }
-        displayOnClick()
-        
-        //creates the book object by taking user input, pushes it into array and calls function to display books onscreen
+
+    // Refresh the displayed books.
+    displayOnClick();
 }
 
-function displayObj(){
-    myLibrary.forEach(element => {
+// Function to display books on the screen.
+function displayObj() {
+    myLibrary.forEach((element, index) => {
         const libIndex = myLibrary.indexOf(element);
         const display = document.querySelector('.display');
         const newDiv = document.createElement('div');
         display.appendChild(newDiv);
-        
+
         const list = document.createElement('ul');
         newDiv.appendChild(list);
 
@@ -54,7 +61,8 @@ function displayObj(){
             const listElement = document.createElement('li');
             const span = document.createElement('span');
             list.appendChild(listElement);
-            listElement.appendChild(span);}
+            listElement.appendChild(span);
+        }
 
         list.childNodes[0].childNodes[0].innerHTML = 'Title:';
         list.childNodes[1].childNodes[0].innerHTML = 'Author:';
@@ -70,52 +78,66 @@ function displayObj(){
         list.childNodes[0].appendChild(title);
         list.childNodes[1].appendChild(author);
         list.childNodes[2].appendChild(year);
-        
+
         const btnDiv = document.createElement('div');
         btnDiv.classList.add('btnDiv');
         newDiv.appendChild(btnDiv);
-        
+
+        // Create a button for marking the book as read/unread.
         const checkSpan = document.createElement('span');
-        checkSpan.classList.add('fa-solid', 'fa-circle-check');
+        checkSpan.classList.add('fa-regular', 'fa-eye');
         checkSpan.style.fontWeight = 400;
         btnDiv.appendChild(checkSpan);
-        
+
+        // Set button text and toggle font weight based on read status.
+        checkSpan.style.fontWeight = element.read ? 900 : 400;
+        checkSpan.addEventListener('click', () => toggleReadStatus(index));
+
+        // Create a button for deleting the book.
         const trashSpan = document.createElement('span');
         trashSpan.classList.add('fa-solid', 'fa-trash-can');
         trashSpan.style.fontWeight = 400;
         btnDiv.appendChild(trashSpan);
-        trashSpan.addEventListener('click', function(){
+
+        // Add an event listener to delete the book when the button is clicked.
+        trashSpan.addEventListener('click', function() {
             myLibrary.splice(libIndex, 1);
-            displayOnClick()
-        })
-    })
-        //pulls each object from array and displays its properties on a div
+            displayOnClick();
+        });
+    });
+    // This function pulls each object from the array and displays its properties on a div.
 }
 
-function showForm(){
+// Function to toggle the visibility of the form for adding books.
+function showForm() {
     document.querySelector('.sidebar').classList.toggle('hidden');
 
-    if (document.querySelector('.shwForm').innerHTML === 'New Book'){
+    if (document.querySelector('.shwForm').innerHTML === 'New Book') {
         document.querySelector('.shwForm').innerHTML = 'Hide Menu';
-    }else if (document.querySelector('.shwForm').innerHTML === 'Hide Menu') {
+    } else if (document.querySelector('.shwForm').innerHTML === 'Hide Menu') {
         document.querySelector('.shwForm').innerHTML = 'New Book';
     }
-    //allows to toggle the entire form area on or off
+    // This function allows toggling the entire form area on or off.
 }
 
-function displayOnClick(){
+// Function to display the library of books and handle initial display.
+function displayOnClick() {
     const sidebar = document.querySelector('.sidebar');
     const display = document.querySelector('.display');
-        
-    if (display.hasChildNodes()){
-        while (display.firstChild){
-            display.removeChild(display.firstChild)
+
+    if (display.hasChildNodes()) {
+        // If there are displayed books, remove them.
+        while (display.firstChild) {
+            display.removeChild(display.firstChild);
         }
-        displayObj()
-    }else {
-        displayObj()
+        displayObj(); // Call displayObj to display the updated books.
+    } else {
+        displayObj(); // Call displayObj to display books when there are none displayed.
+    }
+}
 
-    };
-    //calls the displayObj function on button click
-
+// Function to toggle the read status of a book.
+function toggleReadStatus(index) {
+    myLibrary[index].read = !myLibrary[index].read; // Toggle the read property.
+    displayOnClick(); // Refresh the display.
 }
